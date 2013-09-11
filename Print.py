e50e65251bd1d4b9d2e6f373804c18ff35725ac6
opilane <opilane@PC215-10.(none)>
2013-09-11 11:54:58 +0300

    minu programm

---
+++ Print.py
@@ -0,0 +1,26 @@
+print "tere"
+print 123
+print 3.141532654
+print 1+3
+print 1/3
+print 12*34
+print 6-9
+print 1==2
+print 1==1
+print 1>2
+print 1<2
+print (1.0+2+3)/(4+5+6)
+print ((1.0+2+3)/(4+5+6))/7
+print 2!=1
+print 1<=1
+print 1==2 or 1==1
+print 1==2 and 1==1
+print True and True
+print True and False
+print not True
+if 1>2:
+	print "Halb"
+else:
+	print "Hea"
+
+
