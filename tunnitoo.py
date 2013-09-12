41c942035a6c6a74031272e0800d599df4a3a4c9
opilane <opilane@PC215-10.(none)>
2013-09-12 09:53:42 +0300

    tunnitöö.py

---
+++ tunnitoo.py
@@ -0,0 +1,17 @@
+print 'tere tulemast'
+# tere tulemast
+print 'Mariin\'i'
+# Mariin'i
+print "Mariin'i"
+# Mariin'i
+print '"Tere," ytles ta.'
+# "Tere," ytles ta.
+print "\"Tere,\" ytles ta."
+# "Tere," ytles ta.
+print '"Mariin\'i," ytles ta.'
+# "Mariin'i," ytles ta.
+
+
+
+
+
