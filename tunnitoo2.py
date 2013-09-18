ffbfe50e98c12a9375facf59a21c91fd7f904f8a
opilane <opilane@PC215-10.(none)>
2013-09-18 10:14:22 +0300

    tunnitoo

---
+++ tunnitoo2.py
@@ -0,0 +1,65 @@
+print 'tere tu++lemast'
+# tere tulemast
+print 'Mariin\'i'
+# Mariin'i
+print "Mariin'i"
+# Mariin'i
+print '"Tere," ytles ta.'
+# "Tere," ytles ta.
+print "\"Tere,\" ytles ta."
+# "Tere," ytles ta.
+print '"Mariin\'i," ytles ta.'
+# "Mariin'i," ytles ta.
+word = "TERE" + "tulemast"
+print word
+# TEREtulemast
+word = "TERE" + "tulemast"
+print "<" + word*2 + ">"
+# <TEREtulemastTEREtulemast>
+print "te" "re"
+# tere
+print "te" .strip() + "re"
+# tere
+word = "TERE"
+print word[2]
+# R
+word = "TERE"
+print word[1:3]
+# ER
+word = "Mariin"
+print word[:2]
+# Ma
+word = "Mariin"
+print word [2:]
+# riin
+word = "Headaega"
+print "Tere" + word[1:]
+# Tereeadaega
+word = "Headaega"
+print "Tere" + word[4]
+# Terea
+word = "tere"
+print word[1:100]
+# ere
+word = "tere"
+print word[-1]
+# e
+word = "tere"
+print word[-2]
+# r
+word = "tere"
+print word[-2:]
+# re
+word = "tere"
+print word[:-2]
+# te
+word = "tere"
+print word[-0]
+# t
+s = "teretulemastminupoole"
+len (s)
+# 21
+
+
+
+
