8af3eb2ddb454653bb6c9ffd0334a41c79e79ff8
MariinTasang <mariin.tasang@gmail.com>
2013-09-26 09:12:04 +0300

    Update kodutoo5.py

--- kodutoo5.py
+++ kodutoo5.py
@@ -1,16 +1,13 @@
 from random import randint
 
 klass= ["Art", "Janis", "Egert", "Kerto", "Eveli", "Jasper", "Mario", "Kairo", "Ardo", "Janar", "Katerin", "Rait", "Aulis", "Mariin", "Timo", "Aleks", "Aigar", "Arti" ]
-# print klass
-# print len(klass)
 
-min= 0
-max= len(klass)-1
-n= randint (min,max)
+for i in xrange(100):
+
+n= randint (0,len(klass)-1)
 
 element= klass[n]
 del klass[n]
-# print klass
 
 klass= klass+[element]
 
