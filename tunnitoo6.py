f9321e9b832e7a16c757786e2b9a3e884338ef1f
MariinTasang <mariin.tasang@gmail.com>
2013-10-16 18:34:36 +0300

    Update tunnitoo6.py

--- tunnitoo6.py
+++ tunnitoo6.py
@@ -1,7 +1,10 @@
 from random import randint
 
 kuulid = ["s"]*3 + ["m"]*5 + ["v"]*3
-print kuulid
+# print kuulid
+
+erinevad= 0
+korrad= 0
 
 for i in xrange (100):
 	
@@ -16,7 +19,13 @@ for i in xrange (100):
 	n= randint(min, max)
 	kuul2=kuulid[n]
 	del kuulid[n]
+	
+	if kuul1==kuul2:
+		erinevad=erinevad+1
+	korrad = korrad+1
 
 	kuulid= kuulid+[kuul1]+[kuul2]
 
-print kuulid
+# print kuulid
+
+print 100*erinevad/korrad
