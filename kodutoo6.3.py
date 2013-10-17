b9cb42ce29eb9503728ae8c7e5482a8edb0e42bc
MariinTasang <mariin.tasang@gmail.com>
2013-10-17 15:20:09 +0300

    Update kodutoo6.3.py

--- kodutoo6.3.py
+++ kodutoo6.3.py
@@ -8,3 +8,52 @@ x=0
 while x<10:
 	print "{0:02d}".format(x+10), 
 	x= x + 1
+print ""
+	
+x=0
+while x<10:
+	print "{0:02d}".format(x+20), 
+	x= x + 1
+print ""
+	
+x=0
+while x<10:
+	print "{0:02d}".format(x+30), 
+	x= x + 1
+print ""
+	
+x=0
+while x<10:
+	print "{0:02d}".format(x+40), 
+	x= x + 1
+print ""
+	
+x=0
+while x<10:
+	print "{0:02d}".format(x+50), 
+	x= x + 1
+print ""
+	
+x=0
+while x<10:
+	print "{0:02d}".format(x+60), 
+	x= x + 1
+print ""
+	
+x=0
+while x<10:
+	print "{0:02d}".format(x+70), 
+	x= x + 1
+print ""
+	
+x=0
+while x<10:
+	print "{0:02d}".format(x+80), 
+	x= x + 1
+print ""
+	
+x=0
+while x<10:
+	print "{0:02d}".format(x+90), 
+	x= x + 1
+print ""
