63f65589e4e8b9b09915ca0967b9d97b034d79ad
opilane <opilane@PC215-10.(none)>
2013-10-17 08:24:45 +0300

    kodutoo

---
+++ kodutoo6.py
@@ -0,0 +1,61 @@
+x=int(-1)
+
+while x<9:
+	x=x+1
+	x=str(x)
+	print((x).zfill(2)),
+	x=int(x)
+print ""
+while x<19:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
+print ""
+while x<29:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
+print ""	
+while x<39:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
+print ""
+while x<49:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
+print ""
+while x<59:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
+print ""
+while x<69:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
+print ""
+while x<79:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
+print ""
+while x<89:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
+print ""
+while x<99:
+	x=x+1
+	x=str(x)
+	print ((x).zfill(2)),
+	x=int(x)
