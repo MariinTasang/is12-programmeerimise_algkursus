3dc55c85dd3d7068dd9151bc8785fd1d00873fdb
opilane <opilane@PC215-10.(none)>
2013-10-24 09:45:43 +0300

    tunnitoo

---
+++ tunnitoo9.py
@@ -0,0 +1,16 @@
+def keskvaartus(x1,x2):
+	return (((x1**2)+(x2**2))/2)**0.5
+
+print keskvaartus(8,6)
+
+print ""
+
+
+def rkv(x1,x2):
+	return ((x1*x1+x2*x2)/2)**0.5
+	
+print "rkv= " +str(rkv(3,4))
+
+
+
+
