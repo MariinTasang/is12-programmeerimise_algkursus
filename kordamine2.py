378f03ef32c4d07c76d0c34feb9eb317e234e2fb
opilane <opilane@PC215-10.(none)>
2013-11-20 09:47:58 +0200

    tunnitoo

---
+++ kordamine2.py
@@ -0,0 +1,33 @@
+print int("1")+1
+
+print "" 
+
+muutuja = "teretulemast"
+teksti_pikkus = len(muutuja)
+
+# print teksti_pikkus
+
+print muutuja + " " + ("Teksti pikkus on: ") + str(teksti_pikkus)
+
+# teretulemast Teksti pikkus on: 12
+
+
+print "See tekst on pikem kui 10 marki: " + str(len(muutuja)>10)
+
+# See tekst on pikem kui 10 marki: True
+
+print "See tekst on pikem kui 10 marki: " + str(teksti_pikkus>10)
+
+# See tekst on pikem kui 10 marki: True
+
+print ""
+
+# kuupjuur 27-st
+print 27 ** (1.0/3)
+
+# kaks komakohta
+arvutus=1.234
+print "{:.2f}".format(arvutus)
+
+
+
