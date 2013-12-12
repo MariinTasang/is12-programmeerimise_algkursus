252f2b4243251a6521178c4bdc3a41ebebfd5553
MariinTasang <mariin.tasang@gmail.com>
2013-12-12 09:55:47 +0200

    tunnitoo

--- tunnitoo11.py
+++ tunnitoo11.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 """
 funktsioon mis viib teksti-kursori
@@ -16,3 +17,24 @@ sys.stdout.write( "\033[2J")
 kursor (5, 10, "Tere")
 
 print ""
+
+"""
+funktsioon mis valjastab ekraanile
+kasti servad.
+Funktsiooni parameetrid x, y, w, h
+"""
+
+def kast (x, y, w, h):
+		
+	kursor(x, y, "#")
+	kursor(x+w, y, "#")
+	 
+	kursor(x, y+h, "#")
+	kursor(x+y, y+h, "#")
+	 
+
+kast (10, 5, 5, 10)
+
+sys.stdout.flush()
+
+time.sleep(60)
