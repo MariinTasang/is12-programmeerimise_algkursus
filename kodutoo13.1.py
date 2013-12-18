d34b5c788de3e925bd1be6bc03fc866c23dafea0
MariinTasang <mariin.tasang@gmail.com>
2013-12-18 18:32:04 +0200

    kodutoo

--- kodutoo13.1.py
+++ kodutoo13.1.py
@@ -9,20 +9,17 @@ def kursor(x, y, tekst):
 sys.stdout.write( "\033[2J")
 
 def kast(x, y, w, h):
-	n = 1
-	m = 1
 	
-	while m <= h:
-		kursor(x+m, y, "#")
-		kursor(x+w, y+m, "#")
-		m = m +1
-		
-		while n <= w:
-			kursor(x+n, y+h, "#")
-			kursor(x+y, y, "#")
-			n = n +1
+	m = 1
+	kursor(x, y, "#" *w)
+	kursor(x y+h, "#" *w)
 
-kast (4, 5, 5, 4)
+	while m <= h:
+		kursor(x, y+m, "#")
+		kursor(x+w, y+m, "#")
+		m = m+1
+
+kast (5, 5, 5, 5)
 
 sys.stdout.flush()
 time.sleep(30)
