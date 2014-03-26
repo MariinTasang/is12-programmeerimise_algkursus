be38eb7d49bfbc3fd48975a4d0a19c96667baa88
MariinTasang <mariin.tasang@gmail.com>
2014-03-26 11:44:09 +0200

    Objekt-Orjenteeritud programeerimine

--- kontakt.py
+++ kontakt.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 kontaktid = {}
 kontaktid_maxid = 1
+
+inimesed = {}
+inimesed_maxid = 1
 
 class Kontakt:
 	
@@ -34,12 +39,32 @@ class Kontakt:
 	def omanik_set(self, omanik):
 		self.omanik = omanik
 		
-
+	def load(self, id):
+		if self.id != 0:
+			print "Kontakt load error1"
+			sys.exit(1)
+		if not id in kontaktid:
+			print "Kontakt load error2"
+			sys.exit(1)
+			
+		self.id = id
+		self.nr = kontaktid[id]["number"]
+		self.omanik = kontaktid[id]["omanik"]		
+			
 class Inimene:
 	
-	#def __init__(self):
-		#self.id = "1"
-		#self.nimi = raw_input("Inimese nimi: ")
+	def __init__(self):
+		self.id = 0
+		
+	def save(self):
+		global inimesed_maxid
+		global inimesed
+		if self.id == 0:
+			self.id = inimesed_maxid
+			inimesed_maxid = inimesed_maxid +1
+		voti = self.id
+		vaartus = {"nimi": self.nimi}
+		inimesed[voti] = vaartus
 		
 	def id_get(self):
 		return self.id
@@ -48,7 +73,18 @@ class Inimene:
 		return self.nimi
 		
 	def nimi_set(self, nimi):
-		self.nimi = name
+		self.nimi = nimi
+		
+	def load(self, id):
+		if self.id != 0:
+			print "Inimesed load error1"
+			sys.exit(1)
+		if not id in inimesed:
+			print "Inimesed load error2"
+			sys.exit(1)
+		
+		self.id = id
+		self.nimi = inimesed[id]["nimi"]
 	
 		
 obj1 = Kontakt()
@@ -57,20 +93,19 @@ obj1.omanik_set(3)
 
 obj1.save()
 
+
 obj2 = Kontakt()
-obj2.number_set("345454112")
-obj2.omanik_set(6)
+obj2.load(1)
 
-obj2.save()
+#print kontaktid
 
-print kontaktid
+obj3 = Inimene()
+obj3.nimi_set("Mariin")
 
-#obj2 = Inimene()
+obj3.save()
 
-#print "ID" + obj2.id_get() + ": " + "Inimesele nimega " + obj2.name_get() + " " + "vastab number " +  obj1.number_get()
+obj4 = Inimene()
+obj4.load(1)
 
-#print obj1.id_get()
-#print obj2.id_get()
 
-#print obj1.omanik_get()
-
+#print inimesed
